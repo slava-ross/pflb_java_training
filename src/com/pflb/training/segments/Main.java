0e243699fd6ccb4a94ee5b410b24b1d13355f2b1
Yaroslav Yagodarov <slava-ross@yandex.ru>
2020-02-17 11:15:10 +0400

    improve output messages

--- src/com/pflb/training/segments/Main.java
+++ src/com/pflb/training/segments/Main.java
@@ -7,8 +7,8 @@ public class Main {
         Segment segment1 = new Segment( 1,1,2,2 );
         Segment segment2 = new Segment( -3,0,1,1 );
 
-        System.out.printf( "%.2f%n", segment1.segmentLength() );
-        System.out.printf( "%.2f%n", segment2.segmentLength() );
+        System.out.printf( "First segment length: %.2f%n", segment1.segmentLength() );
+        System.out.printf( "Second segment length: %.2f%n", segment2.segmentLength() );
 
         if ( segment1.segmentEq( segment2 ) ) {
             System.out.printf( "Segments are equal." );
