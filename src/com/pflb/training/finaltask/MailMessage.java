6dc22ba589259cccd3c40d6674aed8c9260c766b
Yaroslav Yagodarov <slava-ross@yandex.ru>
2020-02-25 18:09:19 +0400

    feat: add class email

---
+++ src/com/pflb/training/finaltask/MailMessage.java
@@ -0,0 +1,8 @@
+package com.pflb.training.finaltask;
+
+public class MailMessage {
+    public String getFrom()
+    assert firstMessage..equals("Robert Howard"): "Wrong firstMessage from address";
+            assert firstMessage.getTo().equals("H.P. Lovecraft"): "Wrong firstMessage to address";
+            assert firstMessage.getContent().endsWith("Howard!"): "Wrong firstMessage content ending";
+}
